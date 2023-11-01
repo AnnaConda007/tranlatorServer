673537679e888e633e86d92951464dc44e202b0c
Anna Hrulckova <annahrulkova@yandex.ru>
2023-11-01 17:57:20 +0400

    добавление книги

--- sekrver.js
+++ sekrver.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 const PORT = 3000;
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -12,35 +12,36 @@ const getIAMToken = async () => {
   const endpoint = "https://iam.api.cloud.yandex.net/iam/v1/tokens";
   try {
     const response = await axios.post(endpoint, {
-      yandexPassportOauthToken: "y0_AgAAAAAm8rfnAATuwQAAAADrbscQxsO9b1M3TImbZqUB1rIytXPfuHQ"
+      yandexPassportOauthToken:
+        "y0_AgAAAAAm8rfnAATuwQAAAADrbscQxsO9b1M3TImbZqUB1rIytXPfuHQ",
     });
-      return response.data.iamToken;
+    return response.data.iamToken;
   } catch (error) {
     console.error("Ошибка при обмене OAuth-токена на IAM-токен:", error);
   }
 };
 
 const translator = async (props) => {
-  const apiUrl = "https://translate.api.cloud.yandex.net/translate/v2/translate";
-  console.log(props.IAM_TOKEN)
+  const apiUrl =
+    "https://translate.api.cloud.yandex.net/translate/v2/translate";
+  console.log(props.IAM_TOKEN);
   try {
     const responseAPI = await axios.post(apiUrl, {
       targetLanguageCode: props.targetLanguage,
       texts: [props.word],
       folderId: props.folderId,
       iamToken: props.IAM_TOKEN, // Передаем IAM токен как параметр запроса
-
     });
 
     const data = responseAPI.data;
     console.log(data.translations?.[0]?.text);
-    return data.translations
+    return data.translations;
   } catch (error) {
     //console.error("Ошибка при обращении к API Яндекс.Переводчик", error);
   }
 };
 
-app.post('/getIAMToken', async (req, res) => {
+app.post("/getIAMToken", async (req, res) => {
   try {
     const IAM_TOKEN = await getIAMToken();
     res.json({ iamToken: IAM_TOKEN });
@@ -49,7 +50,7 @@ app.post('/getIAMToken', async (req, res) => {
   }
 });
 
-app.post('/translate', async (req, res) => {
+app.post("/translate", async (req, res) => {
   try {
     const translatedWord = await translator(req.body);
     res.json({ translatedWord });
@@ -57,8 +58,6 @@ app.post('/translate', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
-console.log(`пиздец`);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- utils/createFolderDisk.js
+++ utils/createFolderDisk.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv').config();
 const axios = require('axios');
 
 const createFolder = async ({ res, userId }) => {
- 
   const TOKEN = process.env.YANDEX_QAUTH_TOKEN__DISK;
   const API_ENDPOINT_CREATE_FOLDER = `https://cloud-api.yandex.net/v1/disk/resources`;
+  const API_ENDPOINT_COPY_FILE = `https://cloud-api.yandex.net/v1/disk/resources/copy`;
+
   try {
+    // создание папки
     await axios.put(API_ENDPOINT_CREATE_FOLDER, null, {
       headers: {
         'Authorization': `OAuth ${TOKEN}`
@@ -14,7 +16,21 @@ const createFolder = async ({ res, userId }) => {
         path: `books/${userId}`,
       }
     });
-    res.status(200).send('Book deleted successfully');
+
+    // копирование файла
+    const from = 'general_books/Harry Potter and the Philosopher\'s Stone.txt';
+    const to = 'Harry Potter and the Philosopher\'s Stone.txt';
+    await axios.post(API_ENDPOINT_COPY_FILE, null, {
+      headers: {
+        'Authorization': `OAuth ${TOKEN}`
+      },
+      params: {
+        from: `books/${from}`,
+        path: `books/${userId}/${to}`,
+      }
+    });
+
+    res.status(200).send('Folder created and file copied successfully');
   } catch (error) {
     res.status(500).send(error);
   }
